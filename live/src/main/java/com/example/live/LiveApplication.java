f9d12b99f1504b55b74499bf436efe2721dfa45d
Christian Orozco <chrisorozco305@gmail.com>
2025-05-25 23:08:49 -0400

    fixed bugs

--- live/src/main/java/com/example/live/LiveApplication.java
+++ live/src/main/java/com/example/live/LiveApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class LiveApplication {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World");
+		System.out.println("Goodbye World!");
 		SpringApplication.run(LiveApplication.class, args);
 	}
 
